258dba373ed6e811fba839e838f9e0ee21eb70f8
e-range <e-range@outlook.com>
2025-03-29 15:23:39 +0300

    start

--- TGBooz_bot/Program.cs
+++ TGBooz_bot/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Telegram.Bot;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Telegram.Bot.Types;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+// See https://aka.ms/new-console-template for more information
+
+using var cts = new CancellationTokenSource();
+
+var bot = new TelegramBotClient("7647109657:AAHeU6yOJ6EjaDcUKNFUb6RVFzsbgMyLoPA", cancellationToken: cts.Token);
+var me = await bot.GetMe();
+
+bot.OnError += OnError;
+bot.OnMessage += OnMessage;
+bot.OnUpdate += OnUpdate;
+
+Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
+Console.ReadLine();
+cts.Cancel(); // stop the bot
+
+async Task OnError(Exception exception, HandleErrorSource source)
+{
+    Console.WriteLine(exception);
+}
+
+async Task OnMessage(Message msg, UpdateType type)
+{
+    if (msg.Text == "/start") {
+        await bot.SendMessage(msg.Chat, $"Hello!");
+    }
+}
+
+async Task OnUpdate(Update update)
+{
+    if (update is { CallbackQuery: { } query }) {
+    }
+}
